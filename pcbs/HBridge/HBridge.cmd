78aa2aac205ce3ada32379475283a21b981faa55
BBlakeslee <bmb8610@rit.edu>
2013-06-30 21:25:55 -0400

    Generated pcb layout files and netlists.

---
+++ pcbs/HBridge/HBridge.cmd
@@ -0,0 +1,164 @@
+# Pin name action command file
+
+# Start of element M5
+ChangePinName(M5, 2, D)
+ChangePinName(M5, 3, S)
+ChangePinName(M5, 1, G)
+
+# Start of element M4
+ChangePinName(M4, 2, D)
+ChangePinName(M4, 3, S)
+ChangePinName(M4, 1, G)
+
+# Start of element M3
+ChangePinName(M3, 2, D)
+ChangePinName(M3, 3, S)
+ChangePinName(M3, 1, G)
+
+# Start of element M2
+ChangePinName(M2, 2, D)
+ChangePinName(M2, 3, S)
+ChangePinName(M2, 1, G)
+
+# Start of element M1
+ChangePinName(M1, 2, D)
+ChangePinName(M1, 3, S)
+ChangePinName(M1, 1, G)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element TB4
+ChangePinName(TB4, 2, 2)
+ChangePinName(TB4, 1, 1)
+
+# Start of element R12
+ChangePinName(R12, 2, 2)
+ChangePinName(R12, 1, 1)
+
+# Start of element R11
+ChangePinName(R11, 2, 2)
+ChangePinName(R11, 1, 1)
+
+# Start of element R10
+ChangePinName(R10, 2, 2)
+ChangePinName(R10, 1, 1)
+
+# Start of element R9
+ChangePinName(R9, 2, 2)
+ChangePinName(R9, 1, 1)
+
+# Start of element R8
+ChangePinName(R8, 2, 2)
+ChangePinName(R8, 1, 1)
+
+# Start of element R7
+ChangePinName(R7, 2, 2)
+ChangePinName(R7, 1, 1)
+
+# Start of element R6
+ChangePinName(R6, 2, 2)
+ChangePinName(R6, 1, 1)
+
+# Start of element D2
+ChangePinName(D2, 1, A)
+ChangePinName(D2, 2, C)
+
+# Start of element D4
+ChangePinName(D4, 1, A)
+ChangePinName(D4, 2, C)
+
+# Start of element D5
+ChangePinName(D5, 1, A)
+ChangePinName(D5, 2, C)
+
+# Start of element D3
+ChangePinName(D3, 1, A)
+ChangePinName(D3, 2, C)
+
+# Start of element D1
+ChangePinName(D1, 1, A)
+ChangePinName(D1, 2, C)
+
+# Start of element R5
+ChangePinName(R5, 2, 2)
+ChangePinName(R5, 1, 1)
+
+# Start of element R3
+ChangePinName(R3, 2, 2)
+ChangePinName(R3, 1, 1)
+
+# Start of element R2
+ChangePinName(R2, 2, 2)
+ChangePinName(R2, 1, 1)
+
+# Start of element R4
+ChangePinName(R4, 2, 2)
+ChangePinName(R4, 1, 1)
+
+# Start of element R1
+ChangePinName(R1, 2, 2)
+ChangePinName(R1, 1, 1)
+
+# Start of element VR1
+ChangePinName(VR1, 3, 3)
+ChangePinName(VR1, 2, 2)
+ChangePinName(VR1, 1, 1)
+
+# Start of element TB3
+ChangePinName(TB3, 3, 3)
+ChangePinName(TB3, 2, 2)
+ChangePinName(TB3, 1, 1)
+
+# Start of element IC3
+ChangePinName(IC3, 8, V+)
+ChangePinName(IC3, 7, B_OUT)
+ChangePinName(IC3, 6, B_IN-)
+ChangePinName(IC3, 5, B_IN+)
+ChangePinName(IC3, 4, GND)
+ChangePinName(IC3, 3, A_IN+)
+ChangePinName(IC3, 2, A_IN-)
+ChangePinName(IC3, 1, A_OUT)
+
+# Start of element IC2
+ChangePinName(IC2, 8, VCC)
+ChangePinName(IC2, 7, PB2)
+ChangePinName(IC2, 6, PB1)
+ChangePinName(IC2, 5, PB0)
+ChangePinName(IC2, 4, GND)
+ChangePinName(IC2, 3, PB4)
+ChangePinName(IC2, 2, PB3)
+ChangePinName(IC2, 1, PB5)
+
+# Start of element IC1
+ChangePinName(IC1, 14, VS)
+ChangePinName(IC1, 13, 1Y)
+ChangePinName(IC1, 12, 2Y)
+ChangePinName(IC1, 11, 3Y)
+ChangePinName(IC1, 10, 4Y)
+ChangePinName(IC1, 9, 4B)
+ChangePinName(IC1, 8, 4A)
+ChangePinName(IC1, 7, GND)
+ChangePinName(IC1, 6, 3B)
+ChangePinName(IC1, 5, 3A)
+ChangePinName(IC1, 4, 2B)
+ChangePinName(IC1, 3, 2A)
+ChangePinName(IC1, 2, 1B)
+ChangePinName(IC1, 1, 1A)
+
+# Start of element TB2
+ChangePinName(TB2, 1, 1)
+ChangePinName(TB2, 2, 2)
+
+# Start of element TB1
+ChangePinName(TB1, 1, 1)
+ChangePinName(TB1, 2, 2)

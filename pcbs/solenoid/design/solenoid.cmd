5619fac9246d4d90a2660377c19a4e12c96e98a1
BBlakeslee <bmb8610@rit.edu>
2013-11-01 21:42:35 -0400

    Completed solenoid PCB layout.  Need to adjust component hole sizes.

---
+++ pcbs/solenoid/design/solenoid.cmd
@@ -0,0 +1,74 @@
+# Pin name action command file
+
+# Start of element D1
+ChangePinName(D1, 3, A2)
+ChangePinName(D1, 2, C)
+ChangePinName(D1, 1, A1)
+
+# Start of element D2
+ChangePinName(D2, 3, A2)
+ChangePinName(D2, 2, C)
+ChangePinName(D2, 1, A1)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element D3
+ChangePinName(D3, 1, A)
+ChangePinName(D3, 2, C)
+
+# Start of element R3
+ChangePinName(R3, 2, 2)
+ChangePinName(R3, 1, 1)
+
+# Start of element TB3
+ChangePinName(TB3, 2, 2)
+ChangePinName(TB3, 1, 1)
+
+# Start of element TB4
+ChangePinName(TB4, 2, 2)
+ChangePinName(TB4, 1, 1)
+
+# Start of element TB1
+ChangePinName(TB1, 2, 2)
+ChangePinName(TB1, 1, 1)
+
+# Start of element TB2
+ChangePinName(TB2, 2, 2)
+ChangePinName(TB2, 1, 1)
+
+# Start of element M1
+ChangePinName(M1, 2, D)
+ChangePinName(M1, 3, S)
+ChangePinName(M1, 1, G)
+
+# Start of element R2
+ChangePinName(R2, 2, 2)
+ChangePinName(R2, 1, 1)
+
+# Start of element IC2
+ChangePinName(IC2, 8, VCC)
+ChangePinName(IC2, 7, Vo1)
+ChangePinName(IC2, 6, Vo2)
+ChangePinName(IC2, 5, VEE)
+ChangePinName(IC2, 3, C)
+ChangePinName(IC2, 2, A)
+
+# Start of element R1
+ChangePinName(R1, 2, 2)
+ChangePinName(R1, 1, 1)
+
+# Start of element IC1
+ChangePinName(IC1, 8, V+)
+ChangePinName(IC1, 7, B_OUT)
+ChangePinName(IC1, 6, B_IN-)
+ChangePinName(IC1, 5, B_IN+)
+ChangePinName(IC1, 4, GND)
+ChangePinName(IC1, 3, A_IN+)
+ChangePinName(IC1, 2, A_IN-)
+ChangePinName(IC1, 1, A_OUT)

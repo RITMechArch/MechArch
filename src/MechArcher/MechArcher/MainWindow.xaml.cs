ae5627f1a8fe23f9ab45fb085a826abc3bacc84a
Daniel Wallach <dywallach@dywallach.net>
2013-11-03 15:42:14 -0500

    webcam stuff

--- src/MechArcher/MechArcher/MainWindow.xaml.cs
+++ src/MechArcher/MechArcher/MainWindow.xaml.cs
@@ -11,6 +11,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Expression.Encoder.Devices;
+using WebcamControl;
+using System.IO;
+using System.Drawing.Imaging;
 
 namespace MechArcher
 {
@@ -19,9 +23,63 @@ namespace MechArcher
     /// </summary>
     public partial class MainWindow : Window
     {
+        Config config = new Config();
         public MainWindow()
         {
+            this.Hide();
+            config.Tag = this;
+            config.Show();
+
             InitializeComponent();
+
+            string imgPath = @"C:\WebcamSnapshots";
+
+            if (Directory.Exists(imgPath) == false)
+            {
+                Directory.CreateDirectory(imgPath);
+            }
+
+            WebCamCtrl.ImageDirectory = imgPath;
+            WebCamCtrl.PictureFormat = ImageFormat.Bmp;
+            WebCamCtrl.FrameRate = 30;
+            WebCamCtrl.FrameSize = new System.Drawing.Size(800,600);
+        }
+
+        public void show(Binding bndg_1)
+        {
+            WebCamCtrl.SetBinding(Webcam.VideoDeviceProperty, bndg_1);
+            base.Show();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Display webcam video on control.
+                WebCamCtrl.StartCapture();
+            }
+            catch (Microsoft.Expression.Encoder.SystemErrorException)
+            {
+                MessageBox.Show("Device is in use by another application");
+            }
+        }
+
+        private void SnapshotButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Take snapshot of webcam image.
+            WebCamCtrl.TakeSnapshot();
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            try
+            {
+                config.Close();
+            }
+            catch
+            { }
+
+            base.OnClosing(e);
         }
     }
 }
---
+++ src/MechArcher/MechArcher/QueuePanel.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MechArcher
+{
+    /// <summary>
+    /// Interaction logic for QueuePanel.xaml
+    /// </summary>
+    public partial class QueuePanel : UserControl
+    {
+        public QueuePanel()
+        {
+            InitializeComponent();
+        }
+    }
+}

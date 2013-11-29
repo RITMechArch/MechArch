e68cd8404e268ddc7f8b510d3c4797e174f57f3c
BBlakeslee <bmb8610@rit.edu>
2013-11-29 18:24:48 -0500

    Added activity indicator LED for IR_Tx.  Added outline and screw holes for IR_Tx PCB.

--- pcbs/IR_Tx/design/IR_Tx.cmd
+++ pcbs/IR_Tx/design/IR_Tx.cmd
@@ -1,5 +1,13 @@
 # Pin name action command file
 
+# Start of element D2
+ChangePinName(D2, 2, ANODE)
+ChangePinName(D2, 1, CATHODE)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
 # Start of element D1
 ChangePinName(D1, 1, 1)
 ChangePinName(D1, 3, 3)

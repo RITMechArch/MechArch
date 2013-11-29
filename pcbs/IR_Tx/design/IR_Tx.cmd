3422a661e1f98f439ff185680bb3d6d74899efda
BBlakeslee <bmb8610@rit.edu>
2013-11-28 23:56:24 -0500

    Generated symbols, schematics, and layout needed for transmit side of occlusion sensors.

---
+++ pcbs/IR_Tx/design/IR_Tx.cmd
@@ -0,0 +1,14 @@
+# Pin name action command file
+
+# Start of element D1
+ChangePinName(D1, 1, 1)
+ChangePinName(D1, 3, 3)
+ChangePinName(D1, 2, 2)
+
+# Start of element TB1
+ChangePinName(TB1, 2, 2)
+ChangePinName(TB1, 1, 1)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)

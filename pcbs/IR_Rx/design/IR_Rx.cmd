dafefd187cb40cd45d1dc788d2c977744a786245
BBlakeslee <bmb8610@rit.edu>
2013-12-04 17:35:19 -0500

    Added IR_Rx PCB design files.

---
+++ pcbs/IR_Rx/design/IR_Rx.cmd
@@ -0,0 +1,44 @@
+# Pin name action command file
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element TB2
+ChangePinName(TB2, 2, 2)
+ChangePinName(TB2, 1, 1)
+
+# Start of element TB1
+ChangePinName(TB1, 2, 2)
+ChangePinName(TB1, 1, 1)
+
+# Start of element D2
+ChangePinName(D2, 2, ANODE)
+ChangePinName(D2, 1, CATHODE)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, ccw)
+ChangePinName(R2, 3, cw)
+ChangePinName(R2, 2, wiper)
+
+# Start of element U1
+ChangePinName(U1, 8, V+)
+ChangePinName(U1, 7, B_OUT)
+ChangePinName(U1, 6, B_IN-)
+ChangePinName(U1, 5, B_IN+)
+ChangePinName(U1, 4, GND)
+ChangePinName(U1, 3, A_IN+)
+ChangePinName(U1, 2, A_IN-)
+ChangePinName(U1, 1, A_OUT)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element PD1
+ChangePinName(PD1, 2, 2)
+ChangePinName(PD1, 1, 1)
